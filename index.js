00f1c480eeea8038c53052a4ca828653fa8afebe
Mamadou AMADOU <doumstyle@gmail.com>
2022-01-06 05:17:23 +0100

    Completed lab

--- index.js
+++ index.js
@@ -12,12 +12,56 @@ mongoose
   .connect(MONGODB_URI)
   .then(x => {
     console.log(`Connected to the database: "${x.connection.name}"`);
+
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    Recipe.insertMany(data)
+      .then(recipesFromDb =>  {
+        recipesFromDb.forEach(recipe => console.log(` --> recipe: ${recipe.title}`));
+
+        modifyOneRecipe();
+
+        deleteRecipe();
+
+        setTimeout(() => {
+          closeDatabase();
+        }, 1000);
+      })
+
+      
+      .catch(error => console.error('An error occured while creating a recipe: ', error));
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+// Update the Rigatoni recipe's duratiton
+
+async function modifyOneRecipe() {
+  try {
+    const query = {duration: 220};
+    const found = await Recipe.findOneAndUpdate(query, {duration : 100}, {new: true});
+    console.log('Successfuly updated the recipe: ', found.title, '--> New duration: ',found.duration);
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+async function deleteRecipe() {
+  const query = {title: 'Carrot Cake'};
+  await Recipe.deleteOne(query)
+    .then(dbRes => {
+      console.log('Recipe victim of its success! No longer available.', dbRes);
+    })
+    .catch(error => console.log(error));
+}
+
+async function closeDatabase() {
+  await mongoose.connection.close(() => console.log('CRUD operations completed, connexion closed.'));
+}
+
+
+
+
